a18df637b26e03882f2d67af103b8538690c1e4d
Fernando Lopez <flopez00032@gmail.com>
2022-08-16 11:17:57 -0500

    10 functions complete

---
+++ newjs.js
@@ -0,0 +1,10 @@
+function not(input){
+    return !Boolean(input);
+}
+function addOne(value){
+    return value + 1;
+}
+function isEven(number){
+    return
+
+}
--- solutions.js
+++ solutions.js
@@ -20,26 +20,42 @@
  * and
  * concat
  */
-
+//1
 function isTrue(input){
     return input === true;
 }
+//2
 function isFalse(input){
     return input === false;
 }
+//3
 function not(input){
     return !Boolean(input);
 }
+//4
 function addOne(input){
     return Number(input) +1;
 }
+//5
 function isEven(number){
-    if (number != false);
+    if (number !== Boolean(false));
     return number % 2 === 0;
 }
+//6
 function isIdentical(argument1, argument2){
     return argument1 === argument2;
 }
+//7
 function isEqual(input , input2){
-    return
+    return (input == input2);
+}
+//8
+function or(x,y){
+    return x || y;
+}
+function and(x,y){
+    return x && y;
+}
+function concat(x,y){
+    return x.toString() + y.toString();
 }
